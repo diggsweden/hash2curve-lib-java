11c60b32f308975345b7c39471e495656081a167
Josef Andersson <josef.andersson@digg.se>
2025-12-11 13:18:42 +0100

    chore: update justfile and reuseableci
    Signed-off-by: Josef Andersson <josef.andersson@digg.se>

--- src/main/java/se/digg/crypto/hashtocurve/impl/ShallueVanDeWoestijneMapToCurve.java
+++ src/main/java/se/digg/crypto/hashtocurve/impl/ShallueVanDeWoestijneMapToCurve.java
@@ -36,9 +36,9 @@ public class ShallueVanDeWoestijneMapToCurve implements MapToCurve {
    * Woestijne algorithm, optimized for Weierstrass curves. This implementation adheres to the
    * specifications outlined in RFC 9380, section 6.6.2, and section F.2 for efficient computation.
    *
-   * <p>The method computes the x and y coordinates for the point on the elliptic curve, using
-   * modular arithmetic and auxiliary functions for square root computation and conditional
-   * assignments.
+   * <p>
+   * The method computes the x and y coordinates for the point on the elliptic curve, using modular
+   * arithmetic and auxiliary functions for square root computation and conditional assignments.
    *
    * @param u the input value to be mapped to a point on the elliptic curve
    * @return the computed point on the elliptic curve represented as an ECPoint
